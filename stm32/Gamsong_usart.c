cbbc560eaa634f68f04f1eb1acd75124ac547269
Gyeongmin <kgm1306@pusan.ac.kr>
2017-11-25 03:12:35 +0900

    usart header ver.2(misc.h add)

--- stm32/Gamsong_usart.c
+++ stm32/Gamsong_usart.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <misc.h>
 #include <stm32f10x.h>
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_usart.h>
 #include <stm32f10x_exti.h>
 
+#include <stdio.h>
 #include "Gamsong_usart.h"
 
 
